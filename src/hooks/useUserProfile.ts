67bacf5a2daf5413d6bd989ee202373f5e73a30b
phamvinh203 <vinhnd203@gmail.com>
2025-07-01 16:59:56 +0700

    test conflict

--- src/hooks/useUserProfile.ts
+++ src/hooks/useUserProfile.ts
@@ -1,44 +1,44 @@
 import { useState, useEffect } from "react";
 import { getUser, updateUser } from "../service/UserServices/userService";
 import type { UserProfileData } from "../service/UserServices/UserTypes";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 export const useUserProfile = () => {
-    const [user, setUser] = useState<UserProfileData | null>(null);
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<UserProfileData | null>(null);
+  const [loading, setLoading] = useState(true);
 
-    const loadUser = async () => {
-        try {
-            const data = await getUser();
-            setUser(data);
-        } catch (error) {
-            console.error("Lỗi khi tải thông tin người dùng:", error);
-            toast.error("Không thể tải thông tin người dùng");
-        } finally {
-            setLoading(false);
-        }
-    };
-    const editUser = async (firstName: string, lastName: string) => {
-        try {
-            const updated = await updateUser({ firstName, lastName });
-            setUser(prev => prev ? { ...prev, ...updated } : updated)
-            toast.success("Cập nhật thành công!");
-            return true;
-        } catch (err) {
-            toast.error("Cập nhật thất bại");
-            console.error(err);
-            return false;
-        }
-    };
+  const loadUser = async () => {
+    try {
+      const data = await getUser();
+      setUser(data);
+    } catch (error) {
+      console.error("Lỗi khi tải thông tin người dùng:", error);
+      toast.error("Không thể tải thông tin người dùng");
+    } finally {
+      setLoading(false);
+    }
+  };
+  const editUser = async (firstName: string, lastName: string) => {
+    try {
+      const updated = await updateUser({ firstName, lastName });
+      setUser((prev) => (prev ? { ...prev, ...updated } : updated));
+      toast.success("Cập nhật thành công!hbhjbhjbjhb");
+      return true;
+    } catch (err) {
+      toast.error("Cập nhật thất bại");
+      console.error(err);
+      return false;
+    }
+  };
 
-    useEffect(() => {
-        loadUser();
-    }, [])
+  useEffect(() => {
+    loadUser();
+  }, []);
 
-    return {
-        user,
-        loading,
-        editUser,
-        reload: loadUser,
-    };
+  return {
+    user,
+    loading,
+    editUser,
+    reload: loadUser,
+  };
 };
